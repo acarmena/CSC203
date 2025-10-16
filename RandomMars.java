0547496e733596cf9b5aed19308bf29dcabd9c42
acarmena <camond87979@uat.edu>
2025-10-15 22:10:23 -0700

    Add files via upload

---
+++ RandomMars.java
@@ -0,0 +1,33 @@
+/*
+ * Guidelines:
+As the computer scientist on the Mars Mission, you will be asked to solve problems on Mars.
+This assignment will stretch your abilities to solve problems with code.
+The syntax of the code required is not hard, but solving the requirements will take some thinking!
+
+Expectations / Requirements:
+Your mission is to write a program following these requirements:
+    Welcome the user and provide an overview of your application.
+    Use Random number generation at least 5 times in your program to make it come alive!
+    Create a great User Experience (UX).
+    Over comment your code with your own comments!
+ */
+
+import java.util.random*;//imports the randome function from the java library
+
+public class RandomMars //name/title Red Harvest
+{
+    public static void main (String[] args)//beginging of main method
+    {
+        System.out.println("Welcome to Mars! You're Dr. Lira Halden, the mission's Lead Computational Biologist and Software Engineer!");
+        System.out.println("You have been sent to Mars and stationed in the Ares Biosphere.");
+        System.out.println("This biosphere is Mars' first agricultural and life science outpost.")
+        System.out.println();
+
+        System.out.println("Your job: develop and maintain a system that monitors crop growth, microbial life, and terraforming experiments, while also supporting critical scientific research.");
+        System.out.println("But a mysterious system anomaly is corrupting your experimental data, shutting down sensors, and endangering the mission.");
+        System.out.println("The biosphere's AI, GAIA, begins behaving strangely. Is it Martian interference, sabotage, or something entirely new?");
+        System.out.println();
+
+        Math.random;
+    }
+}
